27ce0854a7ffb895a1f194ec41550d27a3878e04
Akash47a <sakash0120@gmail.com>
2026-06-25 12:55:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
+import RoadMap from './RoadMap'
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Sidebar/>
+      <RoadMap/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <div>Logo</div>
+        <SearchBar/>
+        <div>Level:Newbie</div>
+        <div>Theme</div>
+        <Profile/>
+    </div>
+  )
+}
+
+export default Navbar
