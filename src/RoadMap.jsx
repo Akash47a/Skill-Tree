1a9f39e137cb3b02782bd25d292b212cb0b12f21
Akash47a <sakash0120@gmail.com>
2026-06-29 14:35:05 +0530

    added styles in roadmap

--- src/RoadMap.jsx
+++ src/RoadMap.jsx
@@ -13,11 +13,12 @@ function RoadMap({name}) {
   return (
     <div>
       {datas.length>0?(
-        <div>
+        <div className='roadma'>
           {datas.filter((data) => data.slug == name).map((data)=>(
-            <div key={data.id}>
+            <div key={data.id} className='container'>
+              <div className='course'><p>{data.title} RoadMap</p></div>
               {data[data.slug] && data[data.slug].map((skill) => (
-                <div key={skill.id}>{skill.title}</div>
+                <div key={skill.id}  className='skills'><p>{skill.title}</p></div>
               ))}
             </div>
           ))}
